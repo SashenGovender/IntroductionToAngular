57784c8dccb8596c1b32b6f9acca5ab31b5ef70a
sasheng <sashen50@gmail.com>
2022-11-07 20:53:48 +0200

    added hero service to extract some logic from the heroes component about fetching or knowing about the data

--- TourOfHeroes/src/app/heroes/heroes.component.ts
+++ TourOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Hero } from './../models/hero';
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from './../data/mock-heroes'
+import { HeroService } from '../services/hero.service';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +9,25 @@ import { HEROES } from './../data/mock-heroes'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Array<Hero> = HEROES;
+  heroes: Array<Hero> = [];
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) {
+   // Reserve the constructor for minimal initialization such as wiring constructor parameters to properties. The constructor shouldn't do anything. 
+   }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   public onSelectHero(hero: Hero){
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  public getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes); // The subscribe() method passes the emitted array to the callback, which sets the component's heroes property.
   }
 
 }
---
+++ TourOfHeroes/src/app/services/hero.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HEROES } from '../data/mock-heroes';
+import { Hero } from '../models/hero';
+import { MessageService } from './message.service';
+
+// A provider is something that can create or deliver a service. In this case, it instantiates the HeroService class to provide the service.
+// The injector is the object that chooses and injects the provider where the application requires it.
+// When you provide the service at the root level, Angular creates a single, shared instance of HeroService and injects into any class that asks for it.
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  public getHeroes(): Observable<Array<Hero>> {
+    const heroes = of(HEROES); //of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+}
